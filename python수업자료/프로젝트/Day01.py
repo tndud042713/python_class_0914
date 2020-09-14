4cd6bfa3f358baff67c4de6c2d94ce7347cd67d4
ByunSuyoung <70098543+tndud042713@users.noreply.github.com>
2020-09-14 17:27:04 +0900

    Add files via upload

---
+++ python수업자료/프로젝트/Day01.py
@@ -0,0 +1,32 @@
+# print("Hello python")
+# print("안녕하세요.!")
+# print("내용");print("출력")
+'''
+print("저의 이름은 홍길동 입니다.")
+print('저의 나이는 20살 입니다')
+print('주소는 산골자기 입니다.')
+
+
+print("10+20")
+print(10+20)
+print("10-20")
+print(10-20)
+print("10*20")
+print(10*20)
+print("10/20")
+print(10/20)
+'''
+
+# print("12 + 54 = %d 입니다."%(12+54))
+# print("12 + 54 =", 12+54,"입니다.")
+# print("268 - 42 = %d 입니다."%(268 - 42))
+# print("2 * 23 = %d 입니다."%(2 * 23))
+# print("120 / 3 = %.1f 입니다."%(120 / 3))
+
+# print("두개의 수를 입력하시오")
+# num1 = int(input('정수1 :'))
+# num2 = int(input('정수2 :'))
+# print("두수의 합은 %d 입니다"%(num1+num2))
+print("안녕\r잘가")
+print("안녕\t반갑습니다")
+print("Hello\tPython")
